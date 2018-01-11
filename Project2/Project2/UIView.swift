fecab2d327a893308c9520fac46653178d7881ee
M Wu <dev@mgw.io>
2018-01-11 03:01:18 -0500

    Project 2

---
+++ Project2/Project2/UIView.swift
@@ -0,0 +1,20 @@
+//
+//  UIView.swift
+//  Project2
+//
+//  Created by Michael Wu on 1/10/18.
+//  Copyright © 2018 Michael Wu. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func fadeTransition(_ duration:CFTimeInterval) {
+        let animation = CATransition()
+        animation.timingFunction = CAMediaTimingFunction(name:
+            kCAMediaTimingFunctionEaseInEaseOut)
+        animation.type = kCATransitionFade
+        animation.duration = duration
+        layer.add(animation, forKey: kCATransitionFade)
+    }
+}
